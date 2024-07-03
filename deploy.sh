94fc8a6c06adfd2aebba7ed63315b03c898b14c7
jhan <zealoct@hotmail.com>
2024-07-03 15:47:50 +0800

    use soft link in deploy.sh
    Change-Id: Idc68a4813b39fd04fecab0cb5e42137cecbc1318

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,27 @@
 dir=$(cd $(dirname $BASH_SOURCE[0]) && pwd)
 
 # Copy conf files
-cp $dir/bash/bash_profile $HOME/.bash_profile
-cp $dir/bash/bashrc $HOME/.bashrc
-cp $dir/bash/bash_common $HOME/.bash_common
-cp $dir/zsh/zshrc $HOME/.zshrc
-cp $dir/tmux/tmux.conf $HOME/.tmux.conf
-cp $dir/git/gitconfig $HOME/.gitconfig
+
+rm -f $HOME/.bash_profile && ln -s $dir/bash/bash_profile $HOME/.bash_profile
+rm -f $HOME/.bashrc && ln -s $dir/bash/bashrc $HOME/.bashrc
+rm -f $HOME/.bash_common && ln -s $dir/bash/bash_common $HOME/.bash_common
+rm -f $HOME/.zshrc && ln -s $dir/zsh/zshrc $HOME/.zshrc
+rm -f $HOME/.tmux.conf && ln -s $dir/tmux/tmux.conf $HOME/.tmux.conf
+rm -f $HOME/.gitconfig && ln -s $dir/git/gitconfig $HOME/.gitconfig
 
 mkdir -p $HOME/.mzsh && cp -r $dir/zsh/zsh-git-prompt $HOME/.mzsh/
 
 # Binaries
 mkdir -p $HOME/usr
-cp -r $dir/bin $HOME/usr/
+ln -s $dir/bin $HOME/usr
 
 # vim files
-mkdir -p $HOME/.vim
-cp -r $dir/dot_vim/* $HOME/.vim
+rm -rf $HOME/.vim
+ln -s $dir/dot_vim $HOME/.vim
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
+
+# emacs
+ln -s $dir/emacs $HOME/.emacs.d
 
 # Install jumbo
 bash -c "$( curl http://jumbo.baidu.com/install_jumbo.sh )"
