c070665c8ca5b0c3e9e6c74490fae6371a8388c4
jhan <zealoct@hotmail.com>
2024-07-03 15:47:55 +0800

    rename and update scan_vpn script
    Change-Id: I7f16ea5812e7c581e6c6430c93cf8eaea593aab8

--- bin/scan_vpn.py
+++ bin/scan_vpn.py
@@ -15,14 +15,14 @@ for k in range(23, 24):
     for i in range(232, 234):
         for j in range(0, 256):
             cmd="sshpass -p {} " \
-                "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {}@172.{}.{}.{} " \
-                "\"ls > /dev/null\"".format(passwd, username, k, i, j)
+                "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {}@172.{}.{}.{} \"hostname\"".format(passwd, username, k, i, j)
             #ps[(i, j)] = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             ps[j] = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         for j in range(0, 256):
             ps[j].wait()
             if ps[j].returncode == 0:
-                print "Found 172.{}.{}.{}".format(k, i, j)
+                hostname, _ = ps[j].communicate()
+                print "{} : 172.{}.{}.{}".format(hostname.strip(), k, i, j)
 
 #for i in range(232, 234):
 #    for j in range(0, 256):
