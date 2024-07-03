021622f879b2205ca3d8aa6728e02fd376b6b480
jhan <zealoct@hotmail.com>
2024-07-03 15:47:55 +0800

    [emacs] use tsinghua source
    Change-Id: I906e2043c3f0e245ef172054217a210677d398d9

--- emacs/init-packages.el
+++ emacs/init-packages.el
@@ -6,13 +6,8 @@
 
 ;; set up ELPA and MELPA repositories
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-;; (add-to-list 'package-archives
-;;              '("org" . "http://orgmode.org/elpa/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(setq package-archives '(("gnu-tsinghua" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			 ("melpa-tsinghua" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 
 (package-initialize)
 
