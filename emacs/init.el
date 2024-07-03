298d8c145789ca8651688e216b155905cd83d2e0
jhan <zealoct@hotmail.com>
2024-07-03 15:47:50 +0800

    update emacd.d +yafolding
    Change-Id: I2f46616a308ee8508949b9bdff2e0bc77fd58ca5

--- emacs/init.el
+++ emacs/init.el
@@ -19,12 +19,13 @@
 (if (version< emacs-version "24.4")
     (require 'setup-ivy-counsel)
   (require 'setup-helm)
-  (require 'setup-helm-gtags))
+  (require 'setup-helm-cscope))
+  ;;(require 'setup-helm-gtags))
+;; (require 'setup-ggtags)
 (require 'setup-cedet)
 (require 'setup-editing)
 (require 'setup-c)
 
-(require 'taglist)
 (require 'sr-speedbar)
 (setq speedbar-use-images nil)
 (sr-speedbar-refresh-turn-off)
@@ -38,11 +39,15 @@
 ;; load customized theme
 (load-theme 'zea t)
 
-(add-to-list 'default-frame-alist '(width  . 90))
-(add-to-list 'default-frame-alist '(height . 40))
+(use-package yafolding
+  :config
+  (add-hook 'c-mode-hook 'yafolding-mode)
+  (add-hook 'c++-mode-hook 'yafolding-mode)
+  (define-key yafolding-mode-map (kbd "<C-S-return>") nil)
+  (define-key yafolding-mode-map (kbd "<C-M-return>") nil)
+  (define-key yafolding-mode-map (kbd "<C-return>") nil))
 
-
-;; function-args
+;; element-args
 ;; (require 'function-args)
 ;; (fa-config-default)
 ;; (define-key c-mode-map  [(tab)] 'company-complete)
@@ -57,7 +62,9 @@
     ("ba97e528aa6525b5fb4d9aca64c72eaad024fcc587619e0778bcce3530420de6" default)))
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights
+               use-package undo-tree iedit dtrt-indent counsel-projectile
+               company clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
