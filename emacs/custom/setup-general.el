dcbe72f7691039fade62dc4b8873440039ae39c3
jhan <zealoct@hotmail.com>
2024-07-03 15:47:55 +0800

    set emacs-company idle time to 0
    Change-Id: I62cea4cffb255a491b9505bd83f4ecbd73c9a578

--- emacs/custom/setup-general.el
+++ emacs/custom/setup-general.el
@@ -53,6 +53,8 @@
 (use-package company
   :init
   (global-company-mode 1)
+  (setq company-dabbrev-downcase 0)
+  (setq company-idle-delay 0)
   (delete 'company-semantic company-backends))
 ;; (define-key c-mode-map  [(control tab)] 'company-complete)
 ;; (define-key c++-mode-map  [(control tab)] 'company-complete)
